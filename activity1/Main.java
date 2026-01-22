0323b847983976897cbf0b8f5aff7ba6af5d4121
meliscentaquino <meliscentjhoy.aquino@lorma.edu>
2026-01-23 03:23:22 +0800

    My Fave Books

--- activity1/Main.java
+++ activity1/Main.java
@@ -6,15 +6,15 @@ public class Main {
         faveBook1.isbn = "0063433087";
         faveBook1.publicationDate = "January 14, 2025";
 
-        Book faveBook2 = new Book("A Good Girl's Guide to Murder", "by Holly Jackson", "1405293187", "May 2 2019");
-        faveBook2.title = "A Good Girl's Guide to Murder";
-        faveBook2.author = "Holly Jackson";
+        Book faveBook2 = new Book();
+        faveBook2.title = "\nA Good Girl's Guide to Murder";
+        faveBook2.author = "by Holly Jackson";
         faveBook2.isbn = "1405293187";
         faveBook2.publicationDate = "May 2 2019";
 
-        Book faveBook3 = new Book("The Housemaid", "by Freida McFadden", "1538742578", "August 23, 2022");
+        Book faveBook3 = new Book("\nThe Housemaid", "by Freida McFadden", "1538742578", "August 23, 2022");
 
-        Book faveBook4 = new Book("Say You'll Remember Me", "by Abby Jimenez", "1538759195", "April 1, 2025");
+        Book faveBook4 = new Book("\nSay You'll Remember Me", "by Abby Jimenez", "1538759195", "April 1, 2025");
 
         faveBook1.printBookInfo();
         faveBook2.printBookInfo();
